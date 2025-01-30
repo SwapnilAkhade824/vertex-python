9339317e7a34e8651f0ad6c69c8460c3fd829486
SwapnilAkhade824 <swapnilsantoshakhade@gmail.com>
2025-01-30 20:16:39 +0530

    -

---
+++ password generator.py
@@ -0,0 +1,44 @@
+import random as r
+
+characters = 'abcdefghijklmnopqrstuvwxyz'
+digits = '1234567890'
+symbols = '`~!@#$%^&*|<>?'
+
+password = []
+
+char = int(input("Enter the no of Characters you want : "))
+symb = int(input("Enter the no of symbols you want : "))
+num = int(input("Enter the no of digits you want : "))
+
+total_length = char + symb + num
+
+for i in range (0,total_length-1):
+	if i < char : password.append(r.choice(characters))
+	elif i < (char + symb) : password.append(r.choice(symbols))
+	else : password.append(r.choice(digits))
+
+# Easier Way
+
+password = "".join(password)
+
+print("Your Required Password :",password)
+
+# Harder Way
+
+password = list(password)
+
+harder_password = []
+
+index = []
+
+for i in range (0,total_length-1):
+	ch = r.choice(password)
+	while password.index(ch) in index : 
+		ch = r.choice(password)
+	index.append(password.index(ch))
+	harder_password.append(ch)
+
+harder_password = "".join(harder_password)
+
+print("The Harder Password can be :",harder_password)
+	
