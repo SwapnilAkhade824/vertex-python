0587584bd6f3afeba2f3dcde7fdfcebc81cfad83
SwapnilAkhade824 <swapnilsantoshakhade@gmail.com>
2025-01-30 18:30:38 +0530

    -

---
+++ rockpapersicsorgame.py
@@ -0,0 +1,43 @@
+import random as r
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+game = [rock,paper,scissors]
+
+ch = input("Enter your choice R (rock) P (paper) S (scissors) : ").upper()
+
+if (ch in ['R','P','S']):
+    if(ch == 'R'): ch = 0
+    elif(ch == 'P'): ch = 1
+    else: ch = 2
+    print(f"Your Choice :  {game[ch]}")
+    engch = r.randint(0,len(game)-1)
+    print(f"Engines Choice : {game[engch]}")
+    print("\nResult\n")
+    if(ch==engch): print("Tie")
+    elif(ch == 0 and engch == 2 or ch == 1 and engch == 0 or ch == 2 and engch == 1): print("Win")
+    else: print("Lost")
+else: print("Invalid Choice!!!")
--- use of random module.py
+++ use of random module.py
@@ -20,6 +20,12 @@ import random as r
 # for i in range (1,10):
 # 	print(a[r.randint(0,9)])
 
-people = ['karan','karan']
+# people = ['karan','karan','karan','karan']
 
-print("The person to empty his pocket is",r.choice(people))
+# print("The person to empty his pocket is",r.choice(people))
+
+# marks = [1,2,3,4,50,6,7,8,9,10]
+# m = 0
+# for i in marks:
+# 	if m<i: m=i
+# print("Maximum Marks :",m)
