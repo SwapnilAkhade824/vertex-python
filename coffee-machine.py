7348f7c155bae62db01e62b9721f7dbf74cb3051
SwapnilAkhade824 <swapnilsantoshakhade@gmail.com>
2025-02-19 07:55:27 +0530

    Coffee Machine Added Pricing to the Coffee's Ordered

--- coffee-machine.py
+++ coffee-machine.py
@@ -1,32 +1,41 @@
-# Main Menu:
-# Espresso
-# Cappuccino
-# Latte
-# Americano
-# Mocha
-# Macchiato
-# Hot Water
+# Main Menu (Coffee Selection)
+
+# Espresso – $2.50
+# Cappuccino – $3.50
+# Latte – $4.00
+# Americano – $3.00
+# Mocha – $4.50
+# Macchiato – $3.75
+# Hot Water – $0.50
 # Exit
 
-# Size Selection:
-# Small
-# Medium
-# Large
+# Size Selection (Additional Cost)
 
-# Extra Add-ons:
-# Sugar
-# Milk
-# Extra Shot of Espresso
-# Whipped Cream
-# Caramel Syrup
-# Vanilla Syrup
-# No Add-ons
+# Small – No extra charge
+# Medium – +$0.50
+# Large – +$1.00
+
+# Extra Add-ons (Each item adds to the total price)
+
+# Sugar – Free
+# Milk – +$0.25
+# Extra Shot of Espresso – +$1.00
+# Whipped Cream – +$0.50
+# Caramel Syrup – +$0.75
+# Vanilla Syrup – +$0.75
+# No Add-ons – No extra charge
 
 main_menu = ["Espresso","Cappuccino","Latte","Americano","Mocha","Macchiato","Hot Water","Exit"]
 
+base_price = [2.5, 3.5, 4.3, 4.5, 3.75, 0.5]
+
 size_menu = ["Small","Medium","Large"]
 
+size_price = [0, 0.5, 1]
+
 extra_addons_menu = ["Sugar","Milk","Extra Shot of Espresso","Whipped Cream","Vanilla Syrup","No Add-ons"]
+
+addons_price = [0, 0.25, 1, 0.5]
 
 while True:
 	
@@ -82,6 +91,10 @@ while True:
 
 		continue
 
+	amount = base_price[choice_size-1] + size_price[choice_size-1] + addons_price[choice_addons-1]
+
 	print("\nYour Coffee is Ready: \n")
 
 	print(f"\t{size_menu[choice_size-1]} : {main_menu[choice_coffee-1]} With Added {extra_addons_menu[choice_addons-1]}.\n")
+
+	print(f"\t\tYour Total for The Order : ${amount:.2f}/-\n")
